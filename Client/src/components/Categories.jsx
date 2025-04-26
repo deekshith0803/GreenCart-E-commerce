357425283992dfcd26e94f34fd618ec65362d975
deekshith <deekshithm321@gmail.com>
2025-04-26 13:13:25 +0530

    best seller section done

--- Client/src/components/Categories.jsx
+++ Client/src/components/Categories.jsx
@@ -11,19 +11,17 @@ const Categories = () => {
       <p className='text-2xl md:text-3xl font-medium'>Categories</p>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:gap-6 kl:gap-7 mt-6 gap-6'>
 
-        {categories.map((category, index) => (          <div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col items-center justify-center'
-            style={{ backgroundColor: category.bgColor }}
-            onClick={() =>{
-              navigate(`/products/${category.path.toLowerCase()}`);
-              scrollTo(0, 0);
-            }}
-          >
-            <img src={category.image} alt={category.text} className='group hower:scale-110 transition max-w-28' />
-            <p className='text-sm font-medium'>{category.text}</p>
-          </div>
+        {categories.map((category, index) => (<div key={index} className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col items-center justify-center'
+          style={{ backgroundColor: category.bgColor }}
+          onClick={() => {
+            navigate(`/products/${category.path.toLowerCase()}`);
+            scrollTo(0, 0);
+          }}
+        >
+          <img src={category.image} alt={category.text} className='group hower:scale-110 transition max-w-28' />
+          <p className='text-sm font-medium'>{category.text}</p>
+        </div>
         ))}
-
-
 
       </div>
     </div>
--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,18 +1,66 @@
-import { createContext, use, useContext, useState } from "react";
+import { createContext, use, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+    const currency = import.meta.VITE_CURRENCY
+
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState({});
 
+    // fetch all products
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
 
-    const value = { user, setUser, isSeller, setIsSeller, navigate, showUserLogin, setShowUserLogin };
+    // add product to cart
+    const addToCart = (indexid) => {
+        let cartData = structuredClone(cartItems);
+        if(cartData[indexid]){
+            cartData[indexid] += 1;
+        }else{
+            cartData[indexid] = 1;
+        }
+        setCartItems(cartData);
+        toast.success('Item added to cart');
+    }
+
+    //update cart items quantity
+    const updateCartItems = (indexid, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[indexid] = quantity;
+        setCartItems(cartData);
+        toast.success('Item quantity updated');
+    }
+
+    //remove product from cart
+    const removeFromCart = (indexid) => {
+        let cartData = structuredClone(cartItems);
+        if(cartData[indexid]){
+            cartData[indexid] -= 1;
+            if(cartData[indexid] === 1){
+                delete cartData[indexid];
+            }               
+        }
+        toast.success('Item removed from cart');
+        setCartItems(cartData);
+    }
+ 
+    useEffect(() => {
+        fetchProducts();
+    })
+
+    const value = { user, setUser, isSeller, setIsSeller, navigate, showUserLogin, setShowUserLogin, products, currency ,addToCart, updateCartItems, removeFromCart, cartItems};
     return <AppContext.Provider value={value}>
-        {children}
+        {children} 
     </AppContext.Provider>;
 };
 
